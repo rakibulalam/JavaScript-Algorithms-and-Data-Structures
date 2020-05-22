c6f0afe0b33abfec3f051d12385a7952098f3440
rakibulalam <rakibulalambd@gmail.com>
2020-05-22 17:19:28 +0600

    improving  quick and counting sort

--- sorting/countingSort.js
+++ sorting/countingSort.js
@@ -6,3 +6,26 @@ function countingSort(arr) {
 const arr = [ ...Array(10) ].map((e) => ~~(Math.random() * 10));
 const result = countingSort(arr);
 console.log(result);
+
+/** counting sort with hashtable */
+
+function countingSortWithHash(arr) {
+	/** hash is ordered by default for the number keys */
+	let hash = {};
+	countArray = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (!hash[arr[i]]) {
+			hash[arr[i]] = 1;
+		} else {
+			hash[arr[i]]++;
+		}
+	}
+
+	for (let key in hash) {
+		for (let i = 0; i < hash[key]; i++) {
+			/** you can split the array by the range of the element */
+			countArray.push(key);
+		}
+	}
+	return countArray;
+}
