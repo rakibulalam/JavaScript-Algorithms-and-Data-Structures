2cf7bb4c84dfef9ca7e386fbf781bf018a469c0d
rakibulalam <rakibulalambd@gmail.com>
2020-05-22 23:52:39 +0600

    adding prefix sum algorithom

---
+++ sum/prefixSum.js
@@ -0,0 +1,15 @@
+/**
+ * @description sum algorithm reducing the time complexity for the ranging
+ * @param {*} arr 
+ * @param {*} [[range1],[range2]] 
+ */
+const prefixSum = (arr, [ startIndex, endIndex ]) => {
+	const result = arr.reduce((memo, value, index) => {
+		return [ ...memo, value + (memo[index - 1] || 0) ];
+	}, []);
+	console.log(result, result[startIndex - 1] - result[endIndex]);
+	return result[startIndex - 1] - result[endIndex];
+};
+const arr = [ 1, 2, 3, 4, 5, 6, 7 ];
+
+prefixSum(arr, [ 1, 4 ]);
